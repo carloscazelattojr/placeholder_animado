8b1a5d0abc19836af0d8bb9ba4816f0684f51e65
Carlos Junior <carlosjunior1983@gmail.com>
2019-04-20 18:36:38 -0300

    Exemplo simples e completo de placeholder animado (HTML5)

---
+++ script.js
@@ -0,0 +1,9 @@
+superplaceholder({
+	el: inp3,
+	sentences: ['Digite "PHP"', 'Digite "Java"', 'Digite "C#"', 'Digite "Sua linguagem de programação"'],
+	options: {
+				letterDelay: 80,
+				loop: true,
+				startOnFocus: false
+	}
+})
